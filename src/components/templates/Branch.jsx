7adb9dc60e9d98c9e384bc850b90053b2f1bf5db
feldman11 <arielmfeldman@hotmail.com>
2021-10-09 02:12:50 -0300

    En template Branch, se hace toma de nombre de tienda y pegado de imagen segun seleccion en shop, mediante funcion filterShopSelect

--- src/components/templates/Branch.jsx
+++ src/components/templates/Branch.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import iconoSucursal from "../images/iconoSucursal.png";
 import FooterComp from './FooterComp';
 import accessApi from '../apimethod/accessApi';
 import HeaderComp from './HeaderComp';
 
+function filterShopSelect(objects,attributes,filter){
+    return objects.filter(filter).map(o => attributes.reduce((a,p)=>{
+        a[p]= o[p];
+        return a;
+    },{}));
+}
 
 const Branch = () => {
 
     const shopSelect = localStorage.getItem('shopSelected');
+    const shops = JSON.parse(localStorage.getItem('dataShops'));
 
     accessApi.getBranchOffices(shopSelect);
+
+    const shopSelected = filterShopSelect(shops,['name','imageUrl'],c=>c.id==shopSelect);
     const branchOffices = JSON.parse(localStorage.getItem('dataBranchOffice'));
 
     return (
@@ -20,32 +28,32 @@ const Branch = () => {
                 <div className="txtlocation">
                     <h3>Selecciona sucursal</h3>
                 </div>
-                <div className="branch">
-                    <img src={iconoSucursal} className="imgBranch" alt="Register Address" style={{ position: "relative" }} />
-                    <div className="text">
-                        <h4>Nombre de tienda</h4>
-                        <p>Campo de texto 1 direccion de tienda mas cercana</p>
+                <div className="row">
+                    <div className="col">
+                        <img src={shopSelected[0].imageUrl} className="imgBranch" alt="Register Address" style={{ position: "relative" }} />
+                        <div className="text">
+                            <h4>{shopSelected[0].name}</h4>
+                            <p>{branchOffices[0].city + ', ' + branchOffices[0].street + ' ' + branchOffices[0].numberHeight}</p>
+                        </div>
+                        <a href="/branchconfirm" className="btn" role="button" aria-pressed="true">Seleccionar</a>
                     </div>
-                    <a href="/branchconfirm" className="btn" role="button" aria-pressed="true">Seleccionar</a>
-                </div>
-                <div className="branch">
-                    <img src={iconoSucursal} className="imgBranch" alt="Register Address" style={{ position: "relative" }} />
-                    <div className="text">
-                        <h4>Nombre de tienda</h4>
-                        <p>Campo de texto 2 direccion de tienda mas cercana</p>
+                    <div className="col">
+                        <img src={shopSelected[0].imageUrl} className="imgBranch" alt="Register Address" style={{ position: "relative" }} />
+                        <div className="text">
+                            <h4>{shopSelected[0].name}</h4>
+                            <p>{branchOffices[1].city + ', ' + branchOffices[1].street + ' ' + branchOffices[1].numberHeight}</p>
+                        </div>
+                        <a href="/branchconfirm" className="btn" role="button" aria-pressed="true">Seleccionar</a>
                     </div>
-                    <a href="/branchconfirm" className="btn" role="button" aria-pressed="true">Seleccionar</a>
+                    <div className="col">
+                        <img src={shopSelected[0].imageUrl} className="imgBranch" alt="Register Address" style={{ position: "relative" }} />
+                        <div className="text">
+                            <h4>{shopSelected[0].name}</h4>
+                            <p>{branchOffices[1].city + ', ' + branchOffices[1].street + ' ' + branchOffices[1].numberHeight}</p>
+                        </div>
+                        <a href="/branchconfirm" className="btn" role="button" aria-pressed="true">Seleccionar</a>
+                    </div>                
                 </div>
-
-                <div className="branch">
-                    <img src={iconoSucursal} className="imgBranch" alt="Register Address" style={{ position: "relative" }} />
-                    <div className="text">
-                        <h4>Nombre de tienda</h4>
-                        <p>Campo de texto 3 direccion de tienda mas cercana</p>
-                    </div>
-                    <a href="/branchconfirm" className="btn" role="button" aria-pressed="true">Seleccionar</a>
-                </div>
-
             </div>
             <footer>
                 <FooterComp />
